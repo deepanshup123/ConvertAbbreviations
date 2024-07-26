a3c6c1eca1c72b4807fa3a7b7b243046fe267893
deepanshup123 <149324354+deepanshup123@users.noreply.github.com>
2024-07-27 02:32:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+generate = document.getElementById("button")
+generate.addEventListener("click", glossary_Generate)
+glossary_Text = document.getElementById("exampleFormControlTextarea1")
+
+
+
+function glossary_Generate(){
+	end_abbreviation = "</def-list></glossary>"
+	final_result = "<glossary><title>Abbreviations Used</title><def-list>"
+	user_input = glossary_Text.value.split("\n")
+				     
+	for(let i = 0; i < user_input.length; i++){
+		let def_item = []
+		let forSplitting = "";
+		if (user_input[i].includes("\t")){
+			forSplitting = "\t"
+		}else if (user_input[i].includes("=")){
+			forSplitting = "="
+		}else {
+			let error = document.getElementById("error")
+			error.innerHTML = "!!Please ensure your input of abbreviation must contain '=' or 'tab' Provide a valid Input!!"
+			
+			break;
+		}
+		def_item = user_input[i].split(forSplitting)
+		
+		lists = `<def-item><term id="G${i+1}">${def_item[0].trim()}</term><def><p>${def_item[1].trim()}</p></def></def-item>`
+		final_result = final_result + lists;
+	}
+	result = final_result + end_abbreviation
+	let forUserResult = document.getElementById("exampleFormControlTextarea2")
+			    forUserResult.value = ""
+		end_abbreviation="";
+	final_result=""
+	forUserResult.value = result			    // \t
+		     }
+
+
+				
